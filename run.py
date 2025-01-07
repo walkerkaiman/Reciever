d1380cee998bc874e65f3e6b0d6f26897bd573da
Kaiman Walker <walkerkaiman@gmail.com>
2025-01-06 23:39:50 -0600

    integrated config file

--- run.py
+++ run.py
@@ -4,17 +4,25 @@ pixels = neopixel.NeoPixel()
 receiver = sacn.sACNreceiver()
 
 CONFIG_DATA = {}
+FPS = 30
 
 def main():
     global CONFIG_DATA
     global receiver
     global pixels
+    global FPS
     
     with open('config/config.json') as f:
         CONFIG_DATA = json.load(f)
     
+    FPS = int(CONFIG_DATA['fps'])
+    PIXEL_PIN = CONFIG_DATA['pixel_pin']
+    NUM_OF_LEDS = int(CONFIG_DATA['num_of_leds'])
+    BRIGHTNESS = int(CONFIG_DATA['brightness'])
+    PIXEL_ORDER = CONFIG_DATA['pixel_order']
+    
     pixels = neopixel.NeoPixel(
-    CONFIG_DATA['pixel_pin'], int(CONFIG_DATA['num_of_leds']), brightness=int(CONFIG_DATA['brightness']), auto_write=True, pixel_order=CONFIG_DATA['pixel_order']
+    PIXEL_PIN, NUM_OF_LEDS, brightness=BRIGHTNESS, auto_write=True, pixel_order=PIXEL_ORDER
     )
     print("NeoPixel initialized")
     
@@ -38,17 +46,21 @@ def main():
 # define a callback function
 @receiver.listen_on('universe', universe=1)  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
+    global FPS
+    global pixels
+    
     if packet.dmxStartCode == 0x00:  # ignore non-DMX-data packets
         print(packet.dmxData)  # print the received DMX data
         
         pixels.fill((0, 0, 0)) # turn all LEDs off to clear any previous data
+        
         dmxIndex = 0
-        for pixelIndex in range(int(CONFIG_DATA['num_of_leds'])):
+        for pixelIndex in range(len(pixels)):
             pixels[pixelIndex] = (packet.dmxData[dmxIndex], packet.dmxData[dmxIndex+1], packet.dmxData[dmxIndex+2])
             dmxIndex += 3
         
         pixels.show()
-        time.sleep(1/int(CONFIG_DATA['fps']))
+        time.sleep(1/FPS)
 
 if __name__ == "__main__":
     pass
