30738f511c5b2a1f51e69978a28a2527b0c2e963
Kaiman Walker <walkerkaiman@gmail.com>
2025-03-09 17:34:01 -0500

    update to loop script. not running smooth.

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
             if current_state == "show":
                 time.sleep(0.01)
             else:
-                time.sleep(0.01)
+                time.sleep(0.1)
     except KeyboardInterrupt:
         for uni in universes.values():
             uni["pixels"].fill((0, 0, 0))
